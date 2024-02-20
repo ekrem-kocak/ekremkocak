6a404dc4ecde415f2ab0e6e4a48a467ed5493928
ekrem-kocak <ekrem-kocak@outlook.com>
2024-02-20 21:55:24 +0300

    sidebar fix

--- ui/src/app/v2/states/mobile-nav.state.ts
+++ ui/src/app/v2/states/mobile-nav.state.ts
@@ -2,7 +2,9 @@ import { Action, Selector, State, StateContext } from '@ngxs/store';
 import { MobileNav } from '../models/mobile-nav';
 import { MOBILE_NAV_DEFAULTS as defaults } from '../defaults/mobile-nav.defaults';
 import { ToggleSidebar } from '../actions/mobile-nav.actions';
+import { Injectable } from '@angular/core';
 
+@Injectable()
 @State<MobileNav.State>({
   name: 'MobileNav',
   defaults,
