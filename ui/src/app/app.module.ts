c31bcce827cfd26c99ab84c8a3cf3167f4c8b031
ekrem-kocak <ekrem-kocak@outlook.com>
2024-02-20 21:55:49 +0300

    cursor css

--- ui/src/app/app.module.ts
+++ ui/src/app/app.module.ts
@@ -6,9 +6,10 @@ import { HttpClientModule } from '@angular/common/http';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { NgxsModule } from '@ngxs/store';
+import { CursorComponent } from './shared/components/cursor/cursor.component';
 
 @NgModule({
-  declarations: [AppComponent],
+  declarations: [AppComponent, CursorComponent],
   imports: [
     BrowserModule,
     BrowserAnimationsModule,
--- ui/src/app/shared/shared.module.ts
+++ ui/src/app/shared/shared.module.ts
@@ -1,10 +1,9 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
-import { PrimeNgModule } from './primeng.module';
 
 @NgModule({
   declarations: [],
-  imports: [CommonModule, PrimeNgModule],
-  exports: [CommonModule, PrimeNgModule],
+  imports: [CommonModule],
+  exports: [CommonModule],
 })
 export class SharedModule {}
